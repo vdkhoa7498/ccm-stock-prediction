8c329496c09711931f6bf5d2d92882022b4818ce
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-08-17 16:01:34 +0700

    rnn chart

--- stock_app.py
+++ stock_app.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-
+from datetime import datetime,date,timedelta
 
 app = dash.Dash()
 server = app.server
@@ -15,9 +15,10 @@ server = app.server
 scaler=MinMaxScaler(feature_range=(0,1))
 
 df_nse = pd.read_csv("./stock_data.csv")
-
+df_nse = df_nse.sort_values(by='Date')
 df_nse["Date"]=pd.to_datetime(df_nse.Date,format="%Y-%m-%d")
 df_nse.index=df_nse['Date']
+
 
 
 data=df_nse.sort_index(ascending=True,axis=0)
@@ -30,10 +31,15 @@ for i in range(0,len(data)):
 new_data.index=new_data.Date
 new_data.drop("Date",axis=1,inplace=True)
 
+percent_train = 80
+percent_valid = 100 - percent_train
+cout_percent_train = int(len(new_data)/100*percent_train)
+cout_percent_valid = len(new_data)-cout_percent_train
+
 dataset=new_data.values
 
-train=dataset[0:987,:]
-valid=dataset[987:,:]
+train=dataset[0:cout_percent_train,:]
+valid=dataset[cout_percent_train:,:]
 
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(dataset)
@@ -48,7 +54,9 @@ x_train,y_train=np.array(x_train),np.array(y_train)
 
 x_train=np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
 
-model=load_model("./model_train.h5")
+model_RNN=load_model("./model_RNN.h5")
+model_LSTM=load_model("./model_LSTM.h5")
+
 
 inputs=new_data[len(new_data)-len(valid)-60:].values
 inputs=inputs.reshape(-1,1)
@@ -60,12 +68,16 @@ for i in range(60,inputs.shape[0]):
 X_test=np.array(X_test)
 
 X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
-closing_price=model.predict(X_test)
+closing_price=model_RNN.predict(X_test)
 closing_price=scaler.inverse_transform(closing_price)
+closing_price_=model_LSTM.predict(X_test)
+closing_price_=scaler.inverse_transform(closing_price_)
 
-train=new_data[:987]
-valid=new_data[987:]
+train=new_data[:cout_percent_train]
+valid=new_data[cout_percent_train:]
 valid['Predictions']=closing_price
+valid_=new_data[cout_percent_train:]
+valid_['Predictions']=closing_price_
 
 
 
@@ -99,9 +111,29 @@ app.layout = html.Div([
                     }
 
                 ),
+                html.H2("RNN Predicted closing price",style={"textAlign": "center"}),
+                dcc.Graph(
+                    id="Predicted Data RNN",
+                    figure={
+                        "data":[
+                            go.Scatter(
+                                x=train.index,
+                                y=valid_["Predictions"],
+                                mode='markers'
+                            )
+
+                        ],
+                        "layout":go.Layout(
+                            title='scatter plot',
+                            xaxis={'title':'Date'},
+                            yaxis={'title':'Closing Rate'}
+                        )
+                    }
+
+                ),
                 html.H2("LSTM Predicted closing price",style={"textAlign": "center"}),
                 dcc.Graph(
-                    id="Predicted Data",
+                    id="Predicted Data LSTM",
                     figure={
                         "data":[
                             go.Scatter(
@@ -118,7 +150,7 @@ app.layout = html.Div([
                         )
                     }
 
-                )                
+                ),                
             ])                
 
 
