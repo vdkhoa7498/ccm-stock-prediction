eca7baf50bd744f90150ae4bfac07bfe238f1fe7
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-08-19 22:40:54 +0700

    update model RNN & LSTM, fix stock_app

--- stock_app.py
+++ stock_app.py
@@ -17,14 +17,14 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 
 XGBmodel = XGBRegressor()
-XGBmodel.load_model("xgboost_model.txt")
+XGBmodel.load_model("xgboost_close_model.txt")
 
 app = dash.Dash()
 server = app.server
 
 scaler=MinMaxScaler(feature_range=(0,1))
 
-df_nse = pd.read_csv("./stock_data.csv")
+df_nse = pd.read_csv("./btc.csv")
 df_nse = df_nse.sort_values(by='Date')
 df_nse["Date"]=pd.to_datetime(df_nse.Date,format="%Y-%m-%d")
 df_nse.index=df_nse['Date']
@@ -71,8 +71,8 @@ def train_test_split_plotdata(data, percent):
     n = int(len(data) * (1 - percent))
     return data[n:]
 
-valid = train_test_split_plotdata(new_data,0.1)
-valid["Predictions"] = predictions
+validXGB = train_test_split_plotdata(new_data,0.1)
+validXGB["Predictions"] = predictions
 
 dataset=new_data.values
 
@@ -110,12 +110,17 @@ RNN_closing_price=model_RNN.predict(X_test)
 RNN_closing_price=scaler.inverse_transform(RNN_closing_price)
 LSTM_closing_price=model_LSTM.predict(X_test)
 LSTM_closing_price=scaler.inverse_transform(LSTM_closing_price)
+XGB_closing_price=model_LSTM.predict(X_test)
+XGB_closing_price=scaler.inverse_transform(XGB_closing_price)
 
 train=new_data[:cout_percent_train]
-valid=new_data[cout_percent_train:]
-valid['Predictions']=RNN_closing_price
-valid_=new_data[cout_percent_train:]
-valid_['Predictions']=LSTM_closing_price
+validRNN=new_data[cout_percent_train:]
+validRNN['Predictions']=RNN_closing_price
+validLSTM=new_data[cout_percent_train:]
+validLSTM['Predictions']=LSTM_closing_price
+
+validXGB1=new_data[cout_percent_train:]
+validXGB1['Predictions']=XGB_closing_price
 
 
 
@@ -127,7 +132,7 @@ app.layout = html.Div([
    
     dcc.Tabs(id="tabs", children=[
        
-        dcc.Tab(label='NSE-TATAGLOBAL Stock Data',children=[
+        dcc.Tab(label='Predict Stock Close Price',children=[
             html.Div([
                 html.H2("Actual closing price",style={"textAlign": "center"}),
                 dcc.Graph(
@@ -135,8 +140,8 @@ app.layout = html.Div([
                     figure={
                         "data":[
                             go.Scatter(
-                                x=valid.index,
-                                y=valid["Close"],
+                                x=validRNN.index,
+                                y=validRNN["Close"],
                                 mode='markers'
                             )
 
@@ -155,8 +160,8 @@ app.layout = html.Div([
                     figure={
                         "data":[
                             go.Scatter(
-                                x=valid.index,
-                                y=valid["Predictions"],
+                                x=validRNN.index,
+                                y=validXGB1["Predictions"],
                                 mode='markers'
                             )
 
@@ -175,8 +180,8 @@ app.layout = html.Div([
                     figure={
                         "data":[
                             go.Scatter(
-                                x=valid.index,
-                                y=valid_["Predictions"],
+                                x=validRNN.index,
+                                y=validRNN["Predictions"],
                                 mode='markers'
                             )
 
@@ -195,8 +200,8 @@ app.layout = html.Div([
                     figure={
                         "data":[
                             go.Scatter(
-                                x=valid.index,
-                                y=valid["Predictions"],
+                                x=validLSTM.index,
+                                y=validLSTM["Predictions"],
                                 mode='markers'
                             )
 
@@ -212,106 +217,9 @@ app.layout = html.Div([
             ])                
 
 
-        ]),
-        dcc.Tab(label='Stocks Data', children=[
-            html.Div([
-                html.H1("Stocks High vs Lows", 
-                        style={'textAlign': 'center'}),
-              
-                dcc.Dropdown(id='my-dropdown',
-                             options=[{'label': 'Tesla', 'value': 'TSLA'},
-                                      {'label': 'Apple','value': 'AAPL'}, 
-                                      {'label': 'Facebook', 'value': 'FB'}, 
-                                      {'label': 'Microsoft','value': 'MSFT'}], 
-                             multi=True,value=['FB'],
-                             style={"display": "block", "margin-left": "auto", 
-                                    "margin-right": "auto", "width": "60%"}),
-                dcc.Graph(id='highlow'),
-                html.H1("Stocks Market Volume", style={'textAlign': 'center'}),
-         
-                dcc.Dropdown(id='my-dropdown2',
-                             options=[{'label': 'Tesla', 'value': 'TSLA'},
-                                      {'label': 'Apple','value': 'AAPL'}, 
-                                      {'label': 'Facebook', 'value': 'FB'},
-                                      {'label': 'Microsoft','value': 'MSFT'}], 
-                             multi=True,value=['FB'],
-                             style={"display": "block", "margin-left": "auto", 
-                                    "margin-right": "auto", "width": "60%"}),
-                dcc.Graph(id='volume')
-            ], className="container"),
         ])
-
-
     ])
 ])
-
-
-@app.callback(Output('highlow', 'figure'),
-              [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown):
-    dropdown = {"TSLA": "Tesla","AAPL": "Apple","FB": "Facebook","MSFT": "Microsoft",}
-    trace1 = []
-    trace2 = []
-    for stock in selected_dropdown:
-        trace1.append(
-          go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                     y=df[df["Stock"] == stock]["High"],
-                     mode='lines', opacity=0.7, 
-                     name=f'High {dropdown[stock]}',textposition='bottom center'))
-        trace2.append(
-          go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                     y=df[df["Stock"] == stock]["Low"],
-                     mode='lines', opacity=0.6,
-                     name=f'Low {dropdown[stock]}',textposition='bottom center'))
-    traces = [trace1, trace2]
-    data = [val for sublist in traces for val in sublist]
-    figure = {'data': data,
-              'layout': go.Layout(colorway=["#5E0DAC", '#FF4F00', '#375CB1', 
-                                            '#FF7400', '#FFF400', '#FF0056'],
-            height=600,
-            title=f"High and Low Prices for {', '.join(str(dropdown[i]) for i in selected_dropdown)} Over Time",
-            xaxis={"title":"Date",
-                   'rangeselector': {'buttons': list([{'count': 1, 'label': '1M', 
-                                                       'step': 'month', 
-                                                       'stepmode': 'backward'},
-                                                      {'count': 6, 'label': '6M', 
-                                                       'step': 'month', 
-                                                       'stepmode': 'backward'},
-                                                      {'step': 'all'}])},
-                   'rangeslider': {'visible': True}, 'type': 'date'},
-             yaxis={"title":"Price (USD)"})}
-    return figure
-
-
-@app.callback(Output('volume', 'figure'),
-              [Input('my-dropdown2', 'value')])
-def update_graph(selected_dropdown_value):
-    dropdown = {"TSLA": "Tesla","AAPL": "Apple","FB": "Facebook","MSFT": "Microsoft",}
-    trace1 = []
-    for stock in selected_dropdown_value:
-        trace1.append(
-          go.Scatter(x=df[df["Stock"] == stock]["Date"],
-                     y=df[df["Stock"] == stock]["Volume"],
-                     mode='lines', opacity=0.7,
-                     name=f'Volume {dropdown[stock]}', textposition='bottom center'))
-    traces = [trace1]
-    data = [val for sublist in traces for val in sublist]
-    figure = {'data': data, 
-              'layout': go.Layout(colorway=["#5E0DAC", '#FF4F00', '#375CB1', 
-                                            '#FF7400', '#FFF400', '#FF0056'],
-            height=600,
-            title=f"Market Volume for {', '.join(str(dropdown[i]) for i in selected_dropdown_value)} Over Time",
-            xaxis={"title":"Date",
-                   'rangeselector': {'buttons': list([{'count': 1, 'label': '1M', 
-                                                       'step': 'month', 
-                                                       'stepmode': 'backward'},
-                                                      {'count': 6, 'label': '6M',
-                                                       'step': 'month', 
-                                                       'stepmode': 'backward'},
-                                                      {'step': 'all'}])},
-                   'rangeslider': {'visible': True}, 'type': 'date'},
-             yaxis={"title":"Transactions Volume"})}
-    return figure
 
 
 if __name__=='__main__':
